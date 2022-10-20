8fb64b5438432fc8ceb7686b9b327aa74a3746af
d-g-ivanov <43344012+d-g-ivanov@users.noreply.github.com>
2022-10-20 16:34:18 +0300

    Delete projects/Worked togather directory

--- projects/Worked togather/frontend/src/components/DataGrid/components/DataHeader.js
+++
@@ -1,9 +0,0 @@
-export default function DataHeader({ data }) {
-  return (
-    <tr className="section-header">
-      <td>{data.emp1}</td>
-      <td>{data.emp2}</td>
-      <td colSpan="2">Total time: {data.total}</td>
-    </tr>
-  );
-}
